5aeea2714ca67c9c01c6fc81613079ad51cff009
Gabriel <mzsgabriel@gmail.com>
2023-07-31 14:20:31 -0300

    Início Módulo Funcionário

---
+++ LocadoraDeVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloFuncionario {
+    public class Funcionario: EntidadeBase<Funcionario> {
+
+        
+        public override void Atualizar(Funcionario registro) {
+                Nome = registro.Nome;
+            }
+        public Funcionario() {
+
+            }
+
+        public Funcionario(int id, string nome) : this(nome) {
+                Id = id;
+            }
+        public string Nome { get; set; }
+
+        public DateTime DataAdmissao { get; set; }
+
+        public float Salario { get; set; }
+
+        public Funcionario(string Nome) {
+                this.Nome = Nome;
+            }
+
+
+        
+    }
+}
---
+++ LocadoraDeVeiculos/ModuloFuncionario/TelaFuncionarioForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace LocadoraDeVeiculos.WinApp.ModuloFuncionario {
+    partial class TelaFuncionarioForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ID:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(75, 20);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(30, 23);
+            this.txtId.TabIndex = 10;
+            this.txtId.Text = "0";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(75, 61);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(367, 23);
+            this.txtNome.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nome: ";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(351, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 61);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(254, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 61);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Admissão: ";
+            // 
+            // dateTime
+            // 
+            this.dateTime.Location = new System.Drawing.Point(75, 93);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(155, 23);
+            this.dateTime.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Salário: ";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(75, 127);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(155, 23);
+            this.txtSalario.TabIndex = 15;
+            // 
+            // TelaFuncionarioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 239);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "TelaFuncionarioForm";
+            this.Text = "Cadastro Funcionário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label2;
+        private TextBox txtId;
+        private TextBox txtNome;
+        private Label label1;
+        private Button button2;
+        private Button button1;
+        private Label label3;
+        private DateTimePicker dateTime;
+        private Label label4;
+        private TextBox txtSalario;
+    }
+}
